d432e1f5fe19208241bf39c391422354fe045992
Kien <Kiennguyenvnmos@gmail.com>
2020-08-05 13:48:34 +0300

    utilized

--- randomString.py
+++ randomString.py
@@ -1,29 +1,10 @@
 import random 
-import sys
 
 def randomize (arr, n): 
     for i in range(n-1,1,-1): 
         j = random.randint(1,i) 
         arr[i],arr[j] = arr[j],arr[i] 
     return arr 
-    
-# with open(sys.argv[1], 'r') as file:
-#     text = file.read().replace('\n', '')
-# smallText = text.split(' ')
-# res = ''
-# for arr in smallText:
-#     arr1 = list(arr)
-#     n = len(arr1) 
-#     if('.' in arr1 or ',' in arr1):
-#         temp = ''.join(randomize(arr1, n-1))
-#     else:
-#         temp = ''.join(randomize(arr1, n))
-#     res += temp + ' '
-# print(res)
-
-# text_file = open(sys.argv[2], "w")
-# n = text_file.write(res)
-# text_file.close()
 
 
 def main(text):
--- server.py
+++ server.py
@@ -23,42 +23,14 @@ def upload_file():
 
 @app.route('/getfile', methods=['GET','POST'])
 def getfile():
-    data = {"success": False}
-    # if 'file' not in request.files:
-    #     flash('No file part')
-    # if request.files.get("file"):
-    #     text = request.files["file"].read()
-    #     data["result"] = text
-    #     data["success"] = True
-    # return json.dumps(data)
     if request.method == 'POST':
         file = request.files['file']
-        # f.save(secure_filename(f.filename))       
-        # data["result"] = 'hey'
-        # data["success"] = True
-        # # return 'file uploaded successfully'
-        # return json.dumps(data)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # text = f.read()
-        # print(StringIO.StringIO(text))
-        # return 'file uploaded successfully'
         filename = secure_filename(file.filename)
         myfile = file.read()
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # file.stream.seek(0) # seek to the beginning of file
-        # myfile = file.file 
-        # print(myfile)
-        # return redirect(url_for('uploaded_file',
-        #                         filename=filename))
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         randomStr = randomString.main(myfile.decode("utf-8"))
-        # print(myfile.decode("utf-8")) 
         return randomStr
 
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename)
-    
 if __name__ == "__main__":
 	print("App run!")
 	# Load model
